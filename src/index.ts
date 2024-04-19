41179744addfc493274266e8194267e0d47062c7
Lukas Runge <login@lukasrunge.de>
2024-04-20 00:34:27 +0200

    ✨ make bug reproducible

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { PrismaClient } from '@prisma/client'
+import { PrismaD1 } from '@prisma/adapter-d1'
+
+export interface Env {
+  DB: D1Database
+}
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+    const adapter = new PrismaD1(env.DB)
+    const prisma = new PrismaClient({ adapter })
+
+	const post = await prisma.post.create({
+		data: {
+			title: "Hello World" + Math.random(),
+		}
+	});
+    const user = await prisma.user.create({
+		data: {
+			email: Math.random() + "test@test.de",
+			name: "Test",
+			posts: {
+				connect: {
+					id: post.id
+				}
+			}
+		}
+	});
+    return new Response(JSON.stringify(await prisma.user.findMany({include: {posts: true}})), { status: 200, headers: { 'Content-Type': 'application/json' }})
 	},
 };
